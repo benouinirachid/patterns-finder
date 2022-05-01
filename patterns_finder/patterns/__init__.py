8c616082a5dac15ad41fee440fbd955ded18bd58
rachid benouini <rbenouini@leyton.com>
2022-05-01 16:43:05 +0000

    Add find method

--- patterns_finder/patterns/__init__.py
+++ patterns_finder/patterns/__init__.py
@@ -3,13 +3,13 @@ import regex
 
 
 class Pattern():
-
+    """Pattern class that associate a label with regex."""
     def __init__(self, pattern, label, flags=regex.V0):
         # Compile the regex and init the label
         self.pattern = regex.compile(pattern, flags)
         self.label = label
         # The following initialization will allow the object to act
-        # as an instance of the regex._regex.Pattern
+        # as an instance of the regex._regex.Pattern class
         self.search = self.pattern.search
         self.findall = self.pattern.findall
         self.finditer = self.pattern.finditer
@@ -18,3 +18,14 @@ class Pattern():
         self.scanner = self.pattern.scanner
         self.split = self.pattern.split
         self.splititer = self.pattern.splititer
+
+    def find(self, text):
+        """Search for a pattern on text."""
+        matched_coords = []
+        # find all the occurences of pattern in the text.
+        matches = self.pattern.finditer(text)
+        for match in matches:
+            # get start, end, label and group of text matching the pattern
+            matched_coords.append(
+                (match.span()[0], match.span()[1], self.label, match.group()))
+        return matched_coords
