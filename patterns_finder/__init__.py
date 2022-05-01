ae1204d3427e15783deaf6dbb343c896c0bc63fc
rachid benouini <rbenouini@leyton.com>
2022-05-01 16:41:54 +0000

    Add more details

--- patterns_finder/__init__.py
+++ patterns_finder/__init__.py
@@ -1,2 +1,17 @@
+"""
+Helper functions to construct raw regular expressions strings
+"""
+
+from __future__ import annotations
+from datetime import date
+from . import version
 from . import finder
 from . import patterns
+
+__author__ = "Rachid Benouini"
+__author_email__ = "rachid.benouini@gmail.com"
+__maintainer__ = __author__
+__maintainer_email__ = __author_email__
+__release_date__ = date(year=2022, month=4, day=1)
+__version__ = version.version
+__repository__ = "https://github.com/benouinirachid/patterns-finder"
